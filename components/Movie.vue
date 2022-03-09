a39a004704d7fe8ac2b474d627d885aa18bda163
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-09 18:29:33 +0530

    movie details added

--- components/Movie.vue
+++ components/Movie.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="">
+    <div class="bg-green-400 p-4">
       <div class="relative movie_img">
         <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="" class="object-cover">
         <p class="absolute top-0 left-0 px-4 py-2 bg-red-500 text-white font-bold">{{movie.vote_average}}</p>
-        <div id="movie_details hidden hover:inline" class="bg-white absolute bottom-0 left-0">
-          <h4>{{movie.original_title}}</h4>
-          <small>{{movie.release_date}}</small>
+        <div id="movie_details hidden hover:inline" class="w-full bg-white px-2 absolute bottom-0 left-0">
+          <h4 class="font-bold">{{movie.title}}</h4>
+          <small class="text-gray-500 font-regular">{{movie.release_date}}</small>
         </div>   
       </div>
     </div>
--- components/Movies.vue
+++ components/Movies.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2 mb-6">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10">
         <Movie v-for="(movie, index) in movies" :key="index" class="" :movie=movie />
         <!-- :movie=movie  is our props which passed to Movie component--> 
       </div>
