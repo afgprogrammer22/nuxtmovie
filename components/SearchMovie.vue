638e50d6f801299794c141f2de269263d3d90bca
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-10 20:42:30 +0530

    SearchMovie component added

---
+++ components/SearchMovie.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-1/2" v-if="movies.length > 0">
+    <input type="text" name="search" id="search" placeholder="Search for movie" class="w-full bg-white p-2 focus:outline-none ">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchMovie',
+    computed: {
+        movies() {
+            return this.$store.state.movies
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
