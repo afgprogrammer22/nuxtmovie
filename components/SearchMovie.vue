8643fac1d1e6bbec132899661be36951e6cd942e
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-10 21:52:24 +0530

    added shadow to search field

--- components/SearchMovie.vue
+++ components/SearchMovie.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex w-1/2" v-if="movies.length > 0">
-        <input @keyup.prevent="searchMovies" type="text" name="search" id="search" placeholder="Search for movie" class="w-full bg-white p-2 focus:outline-none rounded-lg">
+        <input @keyup.prevent="searchMovies" type="text" name="search" id="search" placeholder="Search for movie" class="w-full bg-white p-2 focus:outline-none rounded-lg shadow-md">
   </div>
 </template>
 
