03b62c85b63cbb593ae247e086fc8c7fc02307ab
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-03-10 20:44:13 +0530

    Search has been removed from header

--- components/UI/TheHeader.vue
+++ components/UI/TheHeader.vue
@@ -1,15 +1,16 @@
 <template>
   <header class="flex justify-between items-center py-10">
       <nuxt-link to="/" class="text-3xl font-bold border-b-2 border-green-500"><span class="text-green-500">Nuxt</span>Movies</nuxt-link>
-      <div class="w-1/2">
-          <input type="text" name="search" id="search" placeholder="Search for movie" class="w-full bg-white p-2 focus:outline-none">
-      </div>
+      <SearchMovie />
   </header>
 </template>
 
 <script>
 export default {
-    name: 'TheHeader'
+    name: 'TheHeader',
+    
+    
+
 }
 </script>
 
